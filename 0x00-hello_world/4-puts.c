3d79134e9652280e0e10937052899e0bd24c9303
preciousabiakam <Precious Abiakam>
2023-04-21 16:46:06 -0700

    Hello,put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include	<stdio.h>
+/**
+ * main	-	entry point to my code
+ * Return: returns 0 if successful
+ *
+**/
+int	main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
