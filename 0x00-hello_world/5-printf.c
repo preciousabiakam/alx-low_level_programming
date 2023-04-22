92ca5eeb5a0bbb2d74afe0b1db60b19968b36fc3
preciousabiakam <Precious Abiakam>
2023-04-21 17:07:22 -0700

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include	<stdio.h>
+/**
+ * main	-	entry point of my code
+ * Return: returns 0 if successful
+**/
+int	main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
+
