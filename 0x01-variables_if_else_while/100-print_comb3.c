9703d9027d4a113913d85ed95cc01dbaf36af800
preciousabiakam <Precious Abiakam>
2023-04-24 18:40:40 -0700

    Inventing

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print combinations of two digits'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = '0';
+	int m = '0';
+
+	while (n <= '9')
+	{
+		while (m <= '9')
+		{
+			if (!(n > m) || n == m)
+			{
+				putchar(n);
+				putchar(m);
+				if (n == '8' && m == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			m++;
+		}
+		m = '0';
+		n++;
+	}
+	return (0);
+}
