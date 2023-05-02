9161cc0f8f5dd5792505f80027833df3cd84d886
preciousabiakam <Precious Abiakam>
2023-05-02 16:03:32 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_rev -print string in reverse
+ *@s: parameter
+ *Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		_putchar(*(s + len));
+	_putchar(10);
+}
