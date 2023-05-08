8f2f604333d5cb7a0a72412c863d0f63cbc7fa25
preciousabiakam <Precious Abiakam>
2023-05-08 15:19:52 -0700

    Double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - The entry point
+ * @s: input
+ * @to: input
+ * Return: Return 0 when successful
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
